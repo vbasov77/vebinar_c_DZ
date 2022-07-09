3f8b80034f01771386687e90b2eb710282c9b4aa
Vbasov <0120912@mail.ru>
2022-07-09 10:50:52 +0300

    002

---
+++ DZ/002/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа принимает 3 числа, выдаёт максимальное
+
+int a, b, c; 
+Console.WriteLine("Введите 3 числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(b > a){
+ max = b;
+} if (c > max){
+    max = c;
+}
+Console.WriteLine ("Большее число: ");
+ Console.WriteLine (max);
