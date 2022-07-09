ac77938b9cf931d0e6551c2f764ee3d234b7220d
Vbasov <0120912@mail.ru>
2022-07-09 10:20:46 +0300

    001

---
+++ DZ/001/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает два числа, выводит большее
+
+
+int a; 
+int b;
+Console.WriteLine("Введите два числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b){
+Console.WriteLine("{0} большее число", a);
+} else {
+    Console.WriteLine("{0} большее число", b);
+}
+ Console.ReadLine();
+
+
