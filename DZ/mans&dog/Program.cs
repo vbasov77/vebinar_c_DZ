5570ad71cf074b7bf2f707fdc84f15feaf148c96
Vbasov <0120912@mail.ru>
2022-07-09 11:47:54 +0300

    mans&dog

---
+++ DZ/mans&dog/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+int dist = 10000;
+int first_man = 1;
+int second_man = 2;
+int dog = 5;
+int mans = 2;
+int stop = 5;
+int time = 0;
+
+while (dist >= stop) {
+    if (mans == 1) {
+        time = dist / (first_man + dog);
+        mans = 2;
+        dist = dist - (first_man + second_man) * time;
+        count++;
+    }
+    if (mans == 2) {
+        time = dist / (second_man + dog);
+        mans = 1;
+        dist = dist - (first_man + second_man) * time;
+        count++;
+   }
+}
+Console.WriteLine(count);
