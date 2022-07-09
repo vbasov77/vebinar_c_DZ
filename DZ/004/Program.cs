9f7f8d2b3f7425a91790162a2cb926c101aa6050
Vbasov <0120912@mail.ru>
2022-07-09 11:41:30 +0300

    004

---
+++ DZ/004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает n, выводит чётные
+
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+    
+for (int i = 1; i <= n; i++){
+    if (i % 2 == 0){
+        Console.WriteLine(i);
+    }
+}
+
+
