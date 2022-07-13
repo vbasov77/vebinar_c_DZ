c43676f58303ecf113ed0420643972a31f3cd253
Vbasov <0120912@mail.ru>
2022-07-13 09:39:06 +0300

    add 006 lesson

---
+++ DZ/006/Program.cs
@@ -0,0 +1,14 @@
+﻿
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначное число.");
+string number = Console.ReadLine();
+if (number.Length > 2)
+{
+    Console.WriteLine("{0}->{1}", number, number[2]);
+}
+else
+{
+    Console.WriteLine("{0}->{1}", number, "Это не трёхзначное число");
+}
