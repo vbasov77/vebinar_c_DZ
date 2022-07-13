22f9a37d063abfae73b2664773c9cb33c43e3b66
Vbasov <0120912@mail.ru>
2022-07-13 10:14:44 +0300

    add 007 lesson

---
+++ DZ/007/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 6 | number == 7)
+{
+    Console.WriteLine("{0}->{1}", number, "Да");
+}
+else
+{
+    Console.WriteLine("{0}->{1}", number, "Нет");
+}
